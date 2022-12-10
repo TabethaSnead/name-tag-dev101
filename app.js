b3f1996e5ef40d0d5d8a0c47347b9386f563093a
TabethaSnead <tabetha.snead@gmail.com>
2022-12-10 15:47:09 -0800

    added to javaScript

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
+const nameDisplay = document.getElementById('nameDisplay');
+const input = document.getElementById('input');
+const button = document.getElementById('button');
+const nameTag = document.getElementById('nameTag');
+
+button.addEventListener('click', ()=>{
+    console.log('I am clicking the button');
+nameDisplay.textContent = input.value;
+
+
+});
+
 // grab DOM elements
 
 // set event listeners
