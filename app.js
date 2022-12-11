0fb805a10a1f20658f31c63361d69e88366028da
TabethaSnead <tabetha.snead@gmail.com>
2022-12-10 16:34:38 -0800

    adding buttons to nametag

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const nameDisplay = document.getElementById('nameDisplay');
 const input = document.getElementById('input');
 const button = document.getElementById('button');
-const nameTag = document.getElementById('nameTag');
+
+
+const howdy = document.getElementById('howdy');
+const helloChange = document.getElementById('helloChange');
+const helloInput = document.getElementById('helloInput');
+
+
+const pronounInput = document.getElementById('pronounInput');
+const pronounButton = document.getElementById('pronounButton');
+const pronounAnswer = document.getElementById('pronounsAnswer');
 
 button.addEventListener('click', ()=>{
     console.log('I am clicking the button');
@@ -9,6 +18,14 @@ nameDisplay.textContent = input.value;
 
 
 });
+
+helloChange.addEventListener('click', ()=>{
+    howdy.textContent = helloInput.value;
+})
+
+pronounButton.addEventListener('click', ()=>{
+    pronounAnswer.textContent = pronounInput.value;
+})
 
 // grab DOM elements
 
